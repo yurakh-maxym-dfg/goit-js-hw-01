6149245922d5bb67496659498f25afeb59087628
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-02-21 16:26:21 +0200

    task-2 and task-3 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function makeTransaction(quantity, pricePerDroid) {
     let totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
